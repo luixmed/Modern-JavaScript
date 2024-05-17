cae3fb3ae947601f2ccdc058d435d98ba49b366c
Luis Medina <I202215560@cibertec.edu.pe>
2024-05-17 13:32:06 -0500

    refactor: Update getFileName function to handle double-digit numbers and  append file extension
    - Simplify the condition for prefixing '0' to the number if it's less than 9.
    - Concatenate the file extension '.js' to the result.

--- 01-variables-data-types/11-strings/exercises/00-getFileName.js
+++ 01-variables-data-types/11-strings/exercises/00-getFileName.js
@@ -1,14 +1,10 @@
 function getFileName(fileName) {
-  if (!fileName) {
-    throw new Error("Invalid input: fileName cannot be empty");
-  }
-
   const lowerCaseArray = fileName.toLowerCase().split(" ");
+
   let number = +lowerCaseArray[0].slice(0, -1);
+  lowerCaseArray[0] = number < 9 ? `0${number}` : number;
 
-  if (number < 9) lowerCaseArray[0] = `0${number}`;
-
-  return lowerCaseArray.join("-");
+  return lowerCaseArray.join("-").concat(".js");
 }
 
-console.log(getFileName("5: Remove All Occurrences of a Given Character"));
+console.log(getFileName("10: Remove the Letters ABC"));
